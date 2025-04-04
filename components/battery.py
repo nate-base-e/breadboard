ba9a4842dc11bb7c0cf8d80fbe84c9dc5dd6f618
kasey722 <kaseydraper41@gmail.com>
2025-04-04 16:57:37 -0500

    Add Battery class with voltage

---
+++ components/battery.py
@@ -0,0 +1,10 @@
+class Battery:
+
+    def __init__(self, voltage):
+        self.voltage = voltage
+
+    def getVoltage(self):
+        return self.voltage
+
+    def setVoltage(self, voltage):
+        self.voltage = voltage
