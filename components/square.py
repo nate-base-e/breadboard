050485c481449f8cf0404a159700532ceb3a8263
kwalpole <keegan.walpole@grandview.edu>
2025-04-07 14:57:00 -0500

    added square.py file

--- components/square.py
+++ components/square.py
@@ -1,1 +1,19 @@
 #board that other components will be placed on
+#45x80 grid
+#each square 16px
+import pygame as pg
+
+class Square():
+
+    def __init__(self):
+        self.clicked = False
+
+    def draw(self, surface, pos_x, pos_y):
+        pg.draw.rect(surface, (250, 241, 142), (pos_x, pos_y, 16, 16))
+
+
+
+
+
+
+
