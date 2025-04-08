1dd36e95114ee909df2ad8558226ce3fbff62aa9
kasey722 <kaseydraper41@gmail.com>
2025-04-07 21:03:19 -0500

    Adds a simple battery button

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from random import random
 from pygame import mixer
 
 from components.Buttons import Button
-
+from components.battery import Battery
 
 # pygame setup
 
@@ -19,8 +19,12 @@ def main():
     def PushButton():
         print('button pushed')
 
+    def PushBattery():
+        print('battery pushed')
+
     button = Button(680, 500, 140, 52, "Button 1", 32, (45, 45, 45), (100, 100, 100), (100, 100, 100), (0, 0, 0),PushButton)
     ngButton = Button(680, 580, 140, 52, "Button 2", 32, (45, 45, 45), (100, 100, 100), (100, 100, 100), (0, 0, 0), PushButton)
+    battery = Battery(x=100, y=100, width=100, height=40, callback=PushBattery)
     hI = 0
     rollTime = 0
 
@@ -32,7 +36,7 @@ def main():
 
             button.handle_event(event)
             ngButton.handle_event(event)
-
+            battery.handle_event(event)
 
 
             if event.type == pg.KEYDOWN and event.key == pg.K_DOWN:
@@ -41,6 +45,7 @@ def main():
 
         button.draw(screen)
         ngButton.draw(screen)
+        battery.draw(screen)
         pg.display.flip()
 
 if __name__ == "__main__":
