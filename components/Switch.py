932a8e5e278059df8d263d0b83471b198a9717c4
Unchart3d <126183308+Unchart3d@users.noreply.github.com>
2025-04-09 14:00:50 -0500

    switch class

--- components/Switch.py
+++ components/Switch.py
@@ -1,2 +1,21 @@
-def Switch():
-    pass
+import pygame
+
+class Switch:
+    def __init__(self, name="Switch", initial_state=False):
+        self.name = name
+        self.state = initial_state
+
+    def toggle(self):
+        self.state = not self.state
+
+    def turn_on(self):
+        self.state = True
+
+    def turn_off(self):
+        self.state = False
+
+    def is_on(self):
+        return self.state
+
+    def __repr__(self):
+        return f"<{self.name}: {'ON' if self.state else 'OFF'}>"
